01b09f9bfee7c23fe011acef317822e5756acaab
Mirko <ruhlmirkojoel@gmail.com>
2025-02-18 05:06:33 -0300

    App now renders all items that are stored in an array

--- Basic Projects/1-to_do_list/app.js
+++ Basic Projects/1-to_do_list/app.js
@@ -5,20 +5,30 @@ const completeBtn = document.querySelector("#filter-completed");
 */
 
 const addBtn = document.querySelector("#add-button");
+const taskList = document.querySelector("#task-list");
 
 function addTask() {
   const newTask = document.querySelector("#new-task").value.trim();
-  if (newTask) {
-    const taskList = document.querySelector("#task-list");
-    const rawHtml = `<li>
-  <input type='checkbox'>
-  <span>${newTask}</span>
-  <button>Edit</button>
-  <button>Delete</button>
-  </li>`;
-    taskList.insertAdjacentHTML("afterbegin", rawHtml);
-  }
+  listOf.push(newTask);
+  renderTasks();
   document.querySelector("#new-task").value = "";
 }
 
+function renderTasks() {
+  taskList.innerHTML = "";
+  listOf.forEach((item) => getRaw(item));
+}
+
+function getRaw(current) {
+  const rawHtml = `<li>
+  <input type='checkbox'>
+  <span>${current}</span>
+  <button>Edit</button>
+  <button class="delete-btn">Delete</button>
+  </li>`;
+
+  taskList.insertAdjacentHTML("afterbegin", rawHtml);
+}
+
+const listOf = ["first", "second"];
 addBtn.addEventListener("click", addTask);
